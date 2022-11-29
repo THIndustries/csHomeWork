c1ed94314fd70a35562e973d6c85c2726aac15ef
MyName <dragon-negativ@yandex.ru>
2022-11-29 22:01:47 +0300

    написал перувую задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1.
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("Введите первое чило число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе чило число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("Max = " + num1);
+    Console.WriteLine("Min = " + num2);
+}
+else
+{
+    Console.WriteLine("Max = " + num2);
+    Console.WriteLine("Min = " + num1);
+}
+*/
+//-------------------------------------------------------------------------------------------------
+
+//Задача 2.
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2, num3)
+{
+    Console.Write("Максимальное число = " + num1);
+}
