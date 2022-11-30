d43efa4a3c5753d9071efd4ceb425b02421a3f11
MyName <dragon-negativ@yandex.ru>
2022-11-30 13:54:51 +0300

    вторая задача решена

--- Program.cs
+++ Program.cs
@@ -30,7 +30,13 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2, num3)
-{
-    Console.Write("Максимальное число = " + num1);
-}
+int max = num1;
+
+if(num1 > max) 
+    max = num1;
+if(num2 > max)
+    max = num2;
+if(num3 > max)
+    max = num3;
+
+Console.Write("Максимальное число = " + max);
